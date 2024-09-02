2f0353996d4d150b42db78973bf17e49dfd5663f
Sergey Gaidamaka <gsergn4@gmail.com>
2024-09-02 09:40:56 -0600

    skip test wit db call

--- tests/Study.test.ts
+++ tests/Study.test.ts
@@ -1,13 +1,7 @@
 import { test, expect } from '../TestFramework/Fixtures/TestFixture';
 import { faker } from '@faker-js/faker';
 
-test('[CC-T499] Study List - Add Study (CCE)', async ({ cce, sharedActions, db }) => {
-
-  //Sample usage of db fixture
-  const studies = await db.studyRepository.getStudyList(278);
-  console.log(`Study list: \n ${JSON.stringify(studies[0], null, 2)}`);
-  console.log(`Study list id: ${studies[0].studyId}`)
-
+test('[CC-T499] Study List - Add Study (CCE)', async ({ cce, sharedActions }) => {
   //Act
   await cce.studyList.go();
   await cce.studyList.clickAddStudyButton();
@@ -19,4 +13,11 @@ test('[CC-T499] Study List - Add Study (CCE)', async ({ cce, sharedActions, db }
   //Assert
   const studyStatus = await cce.studyDetails.banner.getStudyStatus();
   expect(studyStatus, 'Study has been created but has incorrect default status').toBe('Opportunity');
+});
+
+test.skip('Make a DB call', async ({ db }) => {
+  //Sample usage of db fixture
+  const studies = await db.studyRepository.getStudyList(278);
+  console.log(`Study list: \n ${JSON.stringify(studies[0], null, 2)}`);
+  console.log(`Study list id: ${studies[0].studyId}`);
 });
