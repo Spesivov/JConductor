7db511a39bc5a3b686894412b48e01687ac7845d
Sergey Gaidamaka <gsergn4@gmail.com>
2024-10-20 09:34:54 -0600

    changes

--- TestFramework/Pages/CCS/Study/VisitsForStudyPage.ts
+++ TestFramework/Pages/CCS/Study/VisitsForStudyPage.ts
@@ -1,34 +1,38 @@
 import { Page, Locator } from 'playwright';
 import { StudyVisitPage } from './StudyVisitPage.js';
-import { expect } from '@playwright/test';
+//import { expect } from '@playwright/test';
 
 export class VisitsForStudyPage {
-    private page: Page;
-    private buttonAddNewVisit: Locator;
+  private page: Page;
+  private buttonAddNewVisit: Locator;
 
-    constructor(page: Page) {
-        this.page = page;
-        this.buttonAddNewVisit = page.locator('#CCS_cphMenu_cmdAddVisit');
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.buttonAddNewVisit = page.locator('#CCS_cphMenu_cmdAddVisit');
+  }
 
-    async clickAddNewVisit() {
-        await this.buttonAddNewVisit.click();
-        return new StudyVisitPage(this.page);
-    }
+  async clickApply() {
+    await this.page.locator('#CCS_cphMenu_cmdApply').click();
+  }
 
-    async checkVisit(visitName: string) {
-        console.log(`Checking visit: ${visitName}`);
-        const checkboxVisit = this.page.locator(`//a[.='${visitName}']/../..//input[contains(@id, 'SelectCheckBox')]`);
-        await checkboxVisit.check();
+  async clickAddNewVisit() {
+    await this.buttonAddNewVisit.click();
+    return new StudyVisitPage(this.page);
+  }
 
-        return this;
-    }
+  async checkVisit(visitName: string) {
+    console.log(`Checking visit: ${visitName}`);
+    const checkboxVisit = this.page.locator(`//a[.='${visitName}']/../..//input[contains(@id, 'SelectCheckBox')]`);
+    await checkboxVisit.check();
 
-    async WaitForLoaderDisappear() {
-        const interval = Date.now() + 180 * 1000;
-        while (Date.now() < interval) {
-            try {
-                const loaders = await this.page.isVisible(`
+    return this;
+  }
+
+  async WaitForLoaderDisappear() {
+    const interval = Date.now() + 180 * 1000;
+    while (Date.now() < interval) {
+      try {
+        const loaders = await this.page.isVisible(`
                     //*[contains(@class, 'is-loading')] | 
                     //*[contains(@class, 'raDiv')] | 
                     //div[contains(@id, '_UpdateProgress')] | 
@@ -36,97 +40,101 @@ export class VisitsForStudyPage {
                     //*[@data-testid = 'loadingLogo'] |
                      //*[contains(@class,'skeleton__item')]`);
 
-                if (loaders) {
-                    console.log("Waiting for loader to disappear");
-                    await this.page.waitForTimeout(1000);
-                } else {
-                    console.log("Loader disappeared");
-                    break;
-                }
-            } catch (error) {
-                console.log("Loader dissapeared before checking visibility", error);
-                break;
-            }
+        if (loaders) {
+          console.log('Waiting for loader to disappear');
+          await this.page.waitForTimeout(1000);
+        } else {
+          console.log('Loader disappeared');
+          break;
         }
+      } catch (error) {
+        console.log('Loader dissapeared before checking visibility', error);
+        break;
+      }
     }
+  }
 
-    async selectEditOption(option: string) {
-        console.log(`Selecting Edit Option: ${option}`);
-        const dropdown = this.page.locator(`#CCS_cphMain_cSection_scbEditOptions`);
-        await dropdown.click();
+  async selectEditOption(option: string) {
+    console.log(`Selecting Edit Option: ${option}`);
+    const dropdown = this.page.locator(`#CCS_cphMain_cSection_scbEditOptions`);
+    await dropdown.click();
 
-        const optionItem = this.page.locator(`//label[.='${option}']/input[@type='checkbox']`);
-        await optionItem.check();
+    const optionItem = this.page.locator(`//label[.='${option}']/input[@type='checkbox']`);
+    await optionItem.check();
 
-        return this;
-    }
+    return this;
+  }
 
-    async clickCopyVisit() {
-        console.log('Clicking Copy Visit');
-        // this.page.on('dialog', async dialog => {
-        //     console.log(dialog.message());
+  async clickCopyVisit() {
+    console.log('Clicking Copy Visit');
+    // this.page.on('dialog', async dialog => {
+    //     console.log(dialog.message());
 
-        //     if (dialog.message() === 'Confirm copying of the selected Visits.') {
-        //         await dialog.accept();
-        //     } else {
-        //         await dialog.dismiss();
-        //     }
-        // });
-        // await this.page.click('//input[@value="Copy Visits"]');
-        this.page.click('//input[@value="Copy Visits"]');
-        const dialog = await this.page.waitForEvent('dialog');
+    //     if (dialog.message() === 'Confirm copying of the selected Visits.') {
+    //         await dialog.accept();
+    //     } else {
+    //         await dialog.dismiss();
+    //     }
+    // });
+    // await this.page.click('//input[@value="Copy Visits"]');
 
-        expect(dialog.message()).toBe('Confirm copying of the selected Visits.');
+    this.page.on('dialog', async (dialog) => {
+      console.log(dialog.message());
 
+      if (dialog.message() === 'Confirm copying of the selected Visits.') {
         await dialog.accept();
-        console.log('Confirm dialog accepted');
+      }
+    });
+    await this.page.click('//input[@value="Copy Visits"]');
+    // const dialog = await this.page.waitForEvent('dialog');
 
-        return this;
-    }
+    // expect(dialog.message()).toBe('Confirm copying of the selected Visits.');
 
-    async clickRefresh() {
-        console.log('Clicking Refresh button');
-        const button = this.page.locator('#CCS_cphMain_cSection_cmdRefresh')
-        await button.click();
-    }
+    console.log('Confirm dialog accepted');
 
-    async clickVisitName(visitName: string) {
-        console.log(`Clicking visit: ${visitName}`);
-        const visitLink = this.page.locator(`//a[.='${visitName}']`);
-        await visitLink.click();
+    return this;
+  }
 
-        return this;
-    }
+  async clickRefresh() {
+    console.log('Clicking Refresh button');
+    const button = this.page.locator('#CCS_cphMain_cSection_cmdRefresh');
+    await button.click();
+  }
 
-    async checkAutoRepeat() {
-        console.log('Checking Auto Repeat');
-        const checkbox = this.page.locator('//input[@id="CCS_cphMain_rdRecurrence_C_ckAutoRepeat" and @type="checkbox"]');
-        await checkbox.check();
+  async clickVisitName(visitName: string) {
+    console.log(`Clicking visit: ${visitName}`);
+    const visitLink = this.page.locator(`//a[.='${visitName}']`);
+    await visitLink.click();
 
-        return this;
-    }
+    return this;
+  }
 
-    async fillVisitName(visitName: string, value: string) {
-        console.log(`Filling visit name: ${visitName} with ${value}`);
-        const inputVisitName = this.page.locator(`//input[contains(@id, 'txtEditVisit') and @value='${visitName}']`);
-        await inputVisitName.fill(value);
-    }
+  async checkAutoRepeat() {
+    console.log('Checking Auto Repeat');
+    const checkbox = this.page.locator('//input[@id="CCS_cphMain_rdRecurrence_C_ckAutoRepeat" and @type="checkbox"]');
+    await checkbox.check();
 
-    async selectType(visitName: string, value: string) {
-        console.log(`Selecting Type ${value} for visit: ${visitName}`);
+    return this;
+  }
 
-        const dropdown = this.page.locator(`//input[@value='${visitName}']/../following-sibling::td/div[contains(@id, rcbtype)]`);
-        const option = this.page.locator(`//div[contains(@id, 'rcbType_DropDown') and contains(@style, 'visible')]//text()[contains(., '${value}')]/preceding-sibling::span`);
+  async fillVisitName(visitName: string, value: string) {
+    console.log(`Filling visit name: ${visitName} with ${value}`);
+    const inputVisitName = this.page.locator(`//input[contains(@id, 'txtEditVisit') and @value='${visitName}']`);
+    await inputVisitName.fill(value);
+  }
 
-       // await dropdown.scrollIntoViewIfNeeded();
-        //const input = this.page.locator(`//input[@value='${visitName}']/../following-sibling::td//input[contains(@id, 'rcbType_Input')]`);
-        await dropdown.click();
-        await this.page.waitForTimeout(1000);
+  async selectType(visitName: string, value: string) {
+    console.log(`Selecting Type ${value} for visit: ${visitName}`);
 
-        await option.click();
-        await this.page.waitForTimeout(1000);
-        // await input.fill(value)
+    const dropdown = this.page.locator(
+      `//input[@value='${visitName}']/../following-sibling::td/div[contains(@id, rcbtype)]`
+    );
 
-        // await this.page.keyboard.press('Tab');
-    }
+    const dropDownId = await dropdown.getAttribute('id');
+    const option = this.page.locator(`//div[@id='${dropDownId}_DropDown']//li[contains(., '${value}')]/span`);
+
+    await dropdown.scrollIntoViewIfNeeded();
+    await dropdown.click();
+    await option.click();
+  }
 }
--- tests/StudyLoad.test.ts
+++ tests/StudyLoad.test.ts
@@ -1,71 +1,73 @@
-import { test, expect } from '../TestFramework/Fixtures/TestFixture';
+import { test } from '../TestFramework/Fixtures/TestFixture';
 import { faker } from '@faker-js/faker';
 import { formatDate } from '../TestFramework/Utils/CommonUtils';
 
 test('[CC-T499] Study List - Add Study (CCS)', async ({ ccs, actions }) => {
-    //Arrange
-    const studyName = faker.company.name();
-    const today = formatDate(new Date());
+  //Arrange
+  const studyName = faker.company.name();
+  const today = formatDate(new Date());
 
-    await actions.login.loginAndChooseLocation('automation', 'Password1', 'Aruba Research');
+  await actions.login.loginAndChooseLocation('automation', 'Password1', 'Aruba Research');
 
-    await ccs.headerPanel.enterAndSelectFromResultsInGlobalSearch('Study List');
+  await ccs.headerPanel.enterAndSelectFromResultsInGlobalSearch('Study List');
 
-    //Study creation
-    const studyNewPage = await ccs.studyListPage.clickAddNewStudy();
+  //Study creation
+  const studyNewPage = await ccs.studyListPage.clickAddNewStudy();
 
-    await studyNewPage.fillStudyName(studyName);
-    await studyNewPage.selectSponsor('Abbott');
-    await studyNewPage.selectSite('Aruba Research');
+  await studyNewPage.fillStudyName(studyName);
+  await studyNewPage.selectSponsor('Abbott');
+  await studyNewPage.selectSite('Aruba Research');
 
-    await studyNewPage.expandSection('Target Dates');
-    await studyNewPage.fillSiteInitiationDate(today);
-    await studyNewPage.clickSave();
+  await studyNewPage.expandSection('Target Dates');
+  await studyNewPage.fillSiteInitiationDate(today);
+  await studyNewPage.clickSave();
 
-    //Visit creation
-    const viewStudy = ccs.viewStudyPage;
+  //Visit creation
+  const viewStudy = ccs.viewStudyPage;
 
-    //await viewStudy.waitUntilPageNavigation(viewStudy.getTitle(studyName));
-    await viewStudy.clickStudyLink('Protocol', 'Visits');
-    const newStudyVisit = await ccs.visitsForStudyPage.clickAddNewVisit();
+  //await viewStudy.waitUntilPageNavigation(viewStudy.getTitle(studyName));
+  await viewStudy.clickStudyLink('Protocol', 'Visits');
+  const newStudyVisit = await ccs.visitsForStudyPage.clickAddNewVisit();
 
-    await newStudyVisit.fillVisitName('Screening');
-    await newStudyVisit.fillTargetDate('4', 'Days', '-3', '3');
-    await newStudyVisit.selectSchedulingMode('From Enrollment');
-    await newStudyVisit.selectType('General');
-    await newStudyVisit.clickSave();
+  await newStudyVisit.fillVisitName('Screening');
+  await newStudyVisit.fillTargetDate('4', 'Days', '-3', '3');
+  await newStudyVisit.selectSchedulingMode('From Enrollment');
+  await newStudyVisit.selectType('General');
+  await newStudyVisit.clickSave();
 
-    //Create multiple visits
-    await ccs.visitsForStudyPage.clickAddNewVisit();
-    await newStudyVisit.fillVisitName('Visit#');
-    await newStudyVisit.selectSchedulingMode('From Reference Visit');
-    await newStudyVisit.selectReferenceVisit('1 - Screening');
-    await newStudyVisit.selectType('General');
-    await newStudyVisit.fillCopiesToSave('10');
-    await newStudyVisit.clickSave();
+  //Create multiple visits
+  await ccs.visitsForStudyPage.clickAddNewVisit();
+  await newStudyVisit.fillVisitName('Visit#');
+  await newStudyVisit.selectSchedulingMode('From Reference Visit');
+  await newStudyVisit.selectReferenceVisit('1 - Screening');
+  await newStudyVisit.selectType('General');
+  await newStudyVisit.fillCopiesToSave('10');
+  await newStudyVisit.clickSave();
 
-    const visitForStudy = ccs.visitsForStudyPage;
-    await visitForStudy.checkVisit('Visit1');
-    await visitForStudy.clickCopyVisit();
-    await visitForStudy.WaitForLoaderDisappear();
-    
-    await visitForStudy.selectEditOption('Edit Names');
-    await visitForStudy.selectEditOption('Edit Types');
+  const visitForStudy = ccs.visitsForStudyPage;
+  await visitForStudy.checkVisit('Visit1');
+  await visitForStudy.clickCopyVisit();
+  await visitForStudy.WaitForLoaderDisappear();
 
-    await visitForStudy.clickRefresh();
+  await visitForStudy.selectEditOption('Edit Names');
+  await visitForStudy.selectEditOption('Edit Types');
 
-    await visitForStudy.fillVisitName('Visit1', 'Visit 1 - Arm A');
-    await visitForStudy.fillVisitName('Visit2', 'Visit 2 - Arm A');
-    await visitForStudy.fillVisitName('Visit3', 'Visit 3 - Arm A');
-    await visitForStudy.fillVisitName('Visit4', 'Visit 4 - Arm B');
-    await visitForStudy.fillVisitName('Visit5', 'Visit 5 - Arm B');
-    await visitForStudy.fillVisitName('Visit6', 'Visit 6 - Arm B');
-    await visitForStudy.fillVisitName('Visit7', 'End of Treatment - Both Arms');
-    await visitForStudy.fillVisitName('Visit8', 'Follow up');
-    await visitForStudy.fillVisitName('Visit9', 'Unscheduled');
-    await visitForStudy.fillVisitName('Visit10', 'Ad hoc');
+  await visitForStudy.clickRefresh();
 
-    await visitForStudy.selectType('Screening', 'Screening');
-    await visitForStudy.selectType('Follow up', 'Follow-up');
-    await visitForStudy.selectType('End of Treatment - Both Arms', 'End-of-Treatment');
+  await visitForStudy.fillVisitName('Visit1', 'Visit 1 - Arm A');
+  await visitForStudy.fillVisitName('Visit2', 'Visit 2 - Arm A');
+  await visitForStudy.fillVisitName('Visit3', 'Visit 3 - Arm A');
+  await visitForStudy.fillVisitName('Visit4', 'Visit 4 - Arm B');
+  await visitForStudy.fillVisitName('Visit5', 'Visit 5 - Arm B');
+  await visitForStudy.fillVisitName('Visit6', 'Visit 6 - Arm B');
+  await visitForStudy.fillVisitName('Visit7', 'End of Treatment - Both Arms');
+  await visitForStudy.fillVisitName('Visit8', 'Follow up');
+  await visitForStudy.fillVisitName('Visit9', 'Unscheduled');
+  await visitForStudy.fillVisitName('Visit10', 'Ad hoc');
+
+  await visitForStudy.clickApply();
+
+  await visitForStudy.selectType('Screening', 'Screening');
+  await visitForStudy.selectType('Follow up', 'Follow-up');
+  await visitForStudy.selectType('End of Treatment - Both Arms', 'End-of-Treatment');
 });
