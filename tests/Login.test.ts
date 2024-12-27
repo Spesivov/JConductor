c1f38f91c8506f4362b794951528445f96fffbd1
shaddarion <Sergey.Gaidamaka@advarra.com>
2024-12-27 15:16:44 -0700

    cleanup

--- tests/Login.test.ts
+++ tests/Login.test.ts
@@ -3,8 +3,6 @@ import { test, expect } from '../TestFramework/Fixtures/TestFixture';
 test('User can sign in', async ({ page, actions }) => {
   //Act
   await actions.login.loginAndChooseLocation();
-  const title = page.url();
-  console.log(title);
 
   //Assert
   await expect(page).toHaveTitle('Clinical Conductor CTMS - Welcome', {
